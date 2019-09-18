73403c28e863a0a3e05a9d78560661d095016333
Libo Song <libosong@libos-mbp.lan>
2019-09-17 22:15:05 -0700

    updated by carol

--- gdelt/schema.py
+++ gdelt/schema.py
@@ -1,6 +1,37 @@
 from pyspark.sql.types import *
 from pyspark.sql.types import StringType
 
+class IncidentSchema(object):
+
+    def getIncidentSchema(self):
+        incidentSchema = StructType([
+            StructField('incidntnum',StringType(),True),
+            StructField('incident_date',StringType(),True),
+            StructField('incident_time',StringType(),True),
+            StructField('incident_year',StringType(),True),
+            StructField('incident_day_of_week',StringType(),True),
+            StructField('report_datetime',StringType(),True),
+            StructField('row_id',StringType(),True),
+            StructField('incident_id',StringType(),True),
+            StructField('incident_number',StringType(),True),
+            StructField('cad_number',StringType(),True),
+            StructField('report_type_code',StringType(),True),
+            StructField('report_type_description',StringType(),True),
+            StructField('filed_online',StringType(),True),
+            StructField('incident_code',StringType(),True),
+            StructField('incident_category',StringType(),True),
+            StructField('incident_subcategory',StringType(),True),
+            StructField('incident_description',StringType(),True),
+            StructField('resolution',StringType(),True),
+            StructField('intersection',StringType(),True),
+            StructField('cnn',StringType(),True),
+            StructField('police_district',StringType(),True),
+            StructField('analysis_neighborhood',StringType(),True),
+            StructField('supervisor_district',StringType(),True),
+            StructField('latitude',StringType(),True),
+            StructField('longitude',StringType(),True),
+            StructField('point',StringType(),True)])
+        return incidentSchema
 
 class GDELTDataSchema(object):
 
--- gdelt/spark.sh
+++ gdelt/spark.sh
@@ -7,16 +7,16 @@ JARS=$JARS,/usr/local/spark/lib/postgresql-42.2.5.jar
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 export POSTGRES_HOST='0.0.0.0'
-export POSTGRES_DB='gdelt'
+export POSTGRES_DB='tonebnb'
 export POSTGRES_PORT='5432'
-export POSTGRES_USER='user'
-export POSTGRES_PWD='password'
+export POSTGRES_USER='sa'
+export POSTGRES_PWD='sa'
 
 export PYSPARK_PYTHON=/usr/bin/python3
 
 echo "PATH[spark.sh]: $PROJECT_DIR/spark.sh"
 
-SPARK_MASTER=spark://52.11.103.75:7077
+SPARK_MASTER=spark://54.190.134.137:7077
 SPAKR_SUBMIT=/usr/local/spark/bin/spark-submit
 PY_SPARK=$PROJECT_DIR/spark.py
 
